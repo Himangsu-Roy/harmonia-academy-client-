6ab3985086298e8168a8db9d4a029067b35d76df
himangsu roy <himur98@gmail.com>
2023-06-07 17:22:25 +0600

    music programs section added

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -4,6 +4,7 @@ import Footer from '../components/Footer';
 import Slider from '../components/Slider';
 import PopularClassesSection from '../components/PopularClassesSection';
 import PopularInstructorsSection from '../components/PopularInstructorsSection';
+import MusicProgramsSection from '../components/MusicProgramsSection';
 
 const Main = () => {
     return (
@@ -12,6 +13,7 @@ const Main = () => {
             <Slider/>
             <PopularClassesSection/>
             <PopularInstructorsSection/>
+            <MusicProgramsSection/>
             <Footer/>
         </div>
     );
