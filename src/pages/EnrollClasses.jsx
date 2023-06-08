4329df2d7e775de7f2336d3022533204cca780dc
himangsu roy <himur98@gmail.com>
2023-06-08 15:25:31 +0600

    enroll classes page added

--- src/pages/EnrollClasses.jsx
+++ src/pages/EnrollClasses.jsx
@@ -43,7 +43,6 @@ const EnrollClasses = () => {
 
   return (
     <div className="container mx-auto py-8">
-      <h2 className="text-2xl font-bold mb-4">Student Dashboard</h2>
 
       {/* My Selected Classes */}
       <div>
